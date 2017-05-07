84d5f1d36b3cb136ffed21601738084d40622333
prshnt31191 <chovatiyaprashant0@gmail.com>
2017-05-08 01:22:16 +0530

    c-2

--- ringdroid/app/src/main/java/com/ringdroid/RingdroidSelectActivity.java
+++ ringdroid/app/src/main/java/com/ringdroid/RingdroidSelectActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2008 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.ringdroid;
 
@@ -50,11 +35,6 @@ import android.widget.Toast;
 
 import com.ringdroid.soundfile.SoundFile;
 
-/**
- * Main screen that shows up when you launch Ringdroid. Handles selecting
- * an audio file or using an intent to record a new one, and then
- * launches RingdroidEditActivity from here.
- */
 public class RingdroidSelectActivity
     extends ListActivity
     implements LoaderManager.LoaderCallbacks<Cursor>
--- ringdroid/app/src/main/java/com/ringdroid/WaveformView.java
+++ ringdroid/app/src/main/java/com/ringdroid/WaveformView.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ringdroid;
 
 import android.content.Context;
@@ -30,19 +14,6 @@ import android.view.View;
 
 import com.ringdroid.soundfile.SoundFile;
 
-/**
- * WaveformView is an Android view that displays a visual representation
- * of an audio waveform.  It retrieves the frame gains from a CheapSoundFile
- * object and recomputes the shape contour at several zoom levels.
- *
- * This class doesn't handle selection or any of the touch interactions
- * directly, so it exposes a listener interface.  The class that embeds
- * this view should add itself as a listener and make the view scroll
- * and respond to other events appropriately.
- *
- * WaveformView doesn't actually handle selection, but it will just display
- * the selected part of the waveform in a different color.
- */
 public class WaveformView extends View {
     public interface WaveformListener {
         public void waveformTouchStart(float x);
